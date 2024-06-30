603400238fd6d0a44775e13d5d72999e189c9b48
shreehari-divate <shreeharidivate03@gmail.com>
2024-06-30 14:14:50 +0530

    Few changes in data_ingestion and completed data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the Data ingestion method or component")
         try:
-            df = pd.read_csv('notebook/data/car_pred.csv') # Adjusted for cross-platform compatibility
+            df = pd.read_csv('notebook/data/carpred.csv') # Adjusted for cross-platform compatibility
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
