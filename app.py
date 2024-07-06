da693fb05f37ae0c8a5a2b95db185f42a2b5e99a
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-06 14:08:33 +0530

    updated app

--- app.py
+++ app.py
@@ -17,17 +17,26 @@ def predict():
     if request.method=="POST":
         try:
             car_name=request.form.get("name")
-            data=CustomData(
-                year=request.form.get("year"),
-                kmdriven=request.form.get("kmdriven"),
-                fuel=request.form.get("fuel"),
-                engine=request.form.get('engine'),
-                mileage=request.form.get("mileage"),
-                power=request.form.get("power"),
-                transmission=request.form.get("transmission"),
-                seller=request.form.get("seller"),
-                owner=request.form.get("owner")
-            )
+            year = int(request.form.get("year"))
+            kmdriven = int(request.form.get("kmdriven"))
+            engine = int(request.form.get('engine'))
+            mileage = float(request.form.get("mileage"))
+            power = float(request.form.get("power"))
+            fuel = request.form.get("fuel")
+            transmission = request.form.get("transmission")
+            seller = request.form.get("seller")
+            owner = request.form.get("owner")
+
+            data = CustomData(
+                year=year,
+                kmdriven=kmdriven,
+                fuel=fuel,
+                engine=engine,
+                mileage=mileage,
+                power=power,
+                transmission=transmission,
+                seller=seller,
+                owner=owner)
             pred_df=data.get_data_as_df()
             print("dataframe cols: ",pred_df.columns)
             print(pred_df)
