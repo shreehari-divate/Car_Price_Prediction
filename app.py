c3394c0f2f832ac49f959eb80d9209263fdfd6cc
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-06 13:29:53 +0530

    updated app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
@@ -14,26 +15,30 @@ def index():
 @app.route("/predict",methods=["POST","GET"])
 def predict():
     if request.method=="POST":
-        car_name=request.form.get("name")
-        data=CustomData(
-            year=request.form.get("year"),
-            kmdriven=request.form.get("kmdriven"),
-            fuel=request.form.get("fuel"),
-            engine=request.form.get('engine'),
-            mileage=request.form.get("mileage"),
-            power=request.form.get("power"),
-            transmission=request.form.get("transmission"),
-            seller=request.form.get("seller"),
-            owner=request.form.get("owner")
-        )
-        pred_df=data.get_data_as_df()
-        print("dataframe cols: ",pred_df.columns)
-        print(pred_df)
+        try:
+            car_name=request.form.get("name")
+            data=CustomData(
+                year=request.form.get("year"),
+                kmdriven=request.form.get("kmdriven"),
+                fuel=request.form.get("fuel"),
+                engine=request.form.get('engine'),
+                mileage=request.form.get("mileage"),
+                power=request.form.get("power"),
+                transmission=request.form.get("transmission"),
+                seller=request.form.get("seller"),
+                owner=request.form.get("owner")
+            )
+            pred_df=data.get_data_as_df()
+            print("dataframe cols: ",pred_df.columns)
+            print(pred_df)
 
-        predict_pipeline=PredictPipeline()  
-        results=predict_pipeline.predict(pred_df)[0]
-        res_msg=f"You car {car_name}'s approximate price is Rs. {results:.2f}"
-        return render_template('index.html',results=res_msg) 
+            predict_pipeline=PredictPipeline()  
+            results=predict_pipeline.predict(pred_df)[0]
+            res_msg=f"You car {car_name}'s approximate price is Rs. {results:.2f}"
+            return render_template('index.html',results=res_msg) 
+        except Exception as e:
+            logging.error(f"error occured: {e} ",exc_info=True)
+            return render_template('index.html',results="error occured")
     else:
         return render_template('index.html')
                                     
