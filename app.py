90f750b3fd9093f99a2140bb9accc49dd32e01a8
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-06 13:44:08 +0530

    updated app

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
         return render_template('index.html')
                                     
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
