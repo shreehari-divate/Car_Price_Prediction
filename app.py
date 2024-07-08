8714d19dd1a2a51089ea589d58e019a83bdbd367
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-08 21:33:50 +0530

    fixed app

--- app.py
+++ app.py
@@ -15,39 +15,37 @@ def index():
 @app.route("/predict",methods=["POST","GET"])
 def predict():
     if request.method=="POST":
-        try:
-            car_name=request.form.get("name")
-            year = int(request.form.get("year"))
-            kmdriven = int(request.form.get("kmdriven"))
-            engine = int(request.form.get('engine'))
-            mileage = float(request.form.get("mileage"))
-            power = float(request.form.get("power"))
-            fuel = request.form.get("fuel")
-            transmission = request.form.get("transmission")
-            seller = request.form.get("seller")
-            owner = request.form.get("owner")
+        car_name=request.form.get("name")
+        year = request.form.get("year")
+        kmdriven = request.form.get("kmdriven")
+        engine = request.form.get('engine')
+        mileage = request.form.get("mileage")
+        power = request.form.get("power")
+        fuel = request.form.get("fuel")
+        transmission = request.form.get("transmission")
+        seller = request.form.get("seller")
+        owner = request.form.get("owner")
 
-            data = CustomData(
-                year=year,
-                kmdriven=kmdriven,
-                fuel=fuel,
-                engine=engine,
-                mileage=mileage,
-                power=power,
-                transmission=transmission,
-                seller=seller,
-                owner=owner)
-            pred_df=data.get_data_as_df()
-            print("dataframe cols: ",pred_df.columns)
-            print(pred_df)
+        data = CustomData(
+            year=year,
+            kmdriven=kmdriven,
+            fuel=fuel,
+            engine=engine,
+            mileage=mileage,
+            power=power,
+            transmission=transmission,
+            seller=seller,
+            owner=owner)
+        
+        pred_df=data.get_data_as_df()
+        print("dataframe cols: ",pred_df.columns)
+        print(pred_df)
 
-            predict_pipeline=PredictPipeline()  
-            results=predict_pipeline.predict(pred_df)[0]
-            res_msg=f"You car {car_name}'s approximate price is Rs. {results:.2f}"
-            return render_template('index.html',results=res_msg) 
-        except Exception as e:
-            logging.error(f"error occured: {e} ",exc_info=True)
-            return render_template('index.html',results="error occured")
+        predict_pipeline=PredictPipeline()  
+        results=predict_pipeline.predict(pred_df)[0]
+        res_msg=f"You car {car_name}'s approximate price is Rs. {results:.2f}"
+        return render_template('index.html',results=res_msg) 
+
     else:
         return render_template('index.html')
                                     
