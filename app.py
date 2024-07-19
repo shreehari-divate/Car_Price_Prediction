f3d1cda8c75f280dc094ba27e36e198aa7098d68
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-19 09:54:21 +0530

    added wsgi

--- app.py
+++ app.py
@@ -54,5 +54,3 @@ def predict():
     else:
         return render_template('index.html')
                                     
-if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
